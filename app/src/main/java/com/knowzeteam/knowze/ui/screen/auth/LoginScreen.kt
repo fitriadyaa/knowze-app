dd7fd8144bbf02bddca86b0e1cc86e71e45a9632
Will-Lim1 <will.lim2002@gmail.com>
2023-11-22 15:52:52 +0700

    [Login Screen] Added UI view of Login page

--- app/src/main/java/com/knowzeteam/knowze/ui/screen/auth/LoginScreen.kt
+++ app/src/main/java/com/knowzeteam/knowze/ui/screen/auth/LoginScreen.kt
@@ -2,8 +2,103 @@ package com.knowzeteam.knowze.ui.screen.auth
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.ClickableText
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.knowzeteam.knowze.R
+import com.knowzeteam.knowze.ui.theme.MainColor
+import com.knowzeteam.knowze.ui.theme.PoppinsFontFamily
 
 @Composable
 fun LoginScreen(){
-    Text("Halo")
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Image(
+            painter = painterResource(R.drawable.ic_login),
+            contentDescription = stringResource(R.string.icon_login),
+            modifier = Modifier
+                .size(290.dp, 286.dp)
+        )
+
+        // Button Login with Google
+        Button(
+            onClick = { /* Buat Login with Google */ },
+            shape = RoundedCornerShape(10.dp),
+            modifier = Modifier
+                .size(335.dp, 51.dp),
+            colors = ButtonDefaults.buttonColors(MainColor)
+        ) {
+            Text(
+                text = stringResource(R.string.login_with_google)
+            )
+        }
+
+        // Register ClickableText
+        ClickableText(
+            onClick = { /* Buat Register */ },
+            text = AnnotatedString(
+                stringResource(R.string.register),
+            ),
+            style = TextStyle(
+                color = MainColor,
+                fontSize = 14.sp,
+                fontFamily = PoppinsFontFamily,
+                fontWeight = FontWeight.Medium
+            ),
+            modifier = Modifier
+                .padding(16.dp),
+        )
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.End,
+        verticalArrangement = Arrangement.Bottom
+    ) {
+        // Login with Email ClickableText
+        ClickableText(
+            onClick = { /* Buat Login with Email */ },
+            text = AnnotatedString(
+                stringResource(R.string.login_with_email)
+            ),
+            style = TextStyle(
+                color = MainColor,
+                fontSize = 14.sp,
+                fontFamily = PoppinsFontFamily,
+                fontWeight = FontWeight.Medium,
+                textAlign = TextAlign.End
+            ),
+            modifier = Modifier
+                .padding(16.dp)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LoginScreenPreview() {
+    LoginScreen()
 }
