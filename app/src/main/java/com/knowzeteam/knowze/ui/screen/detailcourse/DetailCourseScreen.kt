8a8a3fbe81239932d63178b2a536ffe95031b4eb
Will-Lim1 <will.lim2002@gmail.com>
2023-11-29 14:00:58 +0700

    [Detail Course Screen] Removed Unused Imports

--- app/src/main/java/com/knowzeteam/knowze/ui/screen/detailcourse/DetailCourseScreen.kt
+++ app/src/main/java/com/knowzeteam/knowze/ui/screen/detailcourse/DetailCourseScreen.kt
@@ -15,10 +15,8 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.KeyboardArrowLeft
-import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
