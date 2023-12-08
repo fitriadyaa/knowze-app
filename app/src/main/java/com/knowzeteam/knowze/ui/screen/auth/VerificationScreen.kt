f31674e41e700b95c7c2d6e8ef7195f1537c1c95
Will-Lim1 <will.lim2002@gmail.com>
2023-12-08 18:32:21 +0700

    [Verification Screen] Slicing UI Verification Screen & Dialog

---
+++ app/src/main/java/com/knowzeteam/knowze/ui/screen/auth/VerificationScreen.kt
@@ -0,0 +1,135 @@
+package com.knowzeteam.knowze.ui.screen.auth
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.ClickableText
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowLeft
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.knowzeteam.knowze.R
+import com.knowzeteam.knowze.ui.component.OTPTextField
+import com.knowzeteam.knowze.ui.theme.KnowzeTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun VerificationScreen(
+    modifier: Modifier = Modifier
+) {
+    val context = LocalContext.current
+    val otpVal: String? = null
+    val focusRequester = remember { FocusRequester() }
+
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = {
+                    Text(
+                        text = stringResource(id = R.string.email_verification),
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
+                    )
+                },
+                navigationIcon = {
+                    IconButton(onClick = { /* do something */ }) {
+                        Icon(
+                            imageVector = Icons.Filled.KeyboardArrowLeft,
+                            contentDescription = "Localized description"
+                        )
+                    }
+                }
+            )
+        },
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = modifier.fillMaxSize()
+        ) {
+            OTPTextField(
+                length = 4,
+                modifier = Modifier
+                    .focusRequester(focusRequester)
+            ) {
+                getOtp -> otpVal
+            }
+
+            Spacer(modifier = Modifier.height(50.dp))
+
+            VerifyButton(onClick = { /*TODO*/ })
+
+            Spacer(modifier = Modifier.height(25.dp))
+
+            ClickableText(
+                onClick = { /*TODO*/ },
+                text = AnnotatedString(stringResource(id = R.string.verify_message)),
+                style = MaterialTheme.typography.bodyMedium.copy(
+                    fontSize = 12.sp,
+                    fontWeight = FontWeight.Light,
+                    color = MaterialTheme.colorScheme.primary
+                )
+            )
+        }
+    }
+}
+
+@Composable
+fun VerifyButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Button(
+        onClick = onClick,
+        shape = RoundedCornerShape(10.dp),
+        modifier = modifier
+            .fillMaxWidth()
+            .height(60.dp),
+        colors = ButtonDefaults.buttonColors(MaterialTheme.colorScheme.primary)
+    ) {
+        Text(
+            text = stringResource(R.string.verify),
+            modifier = modifier.padding(start = 8.dp)
+        )
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun VerificationPreview() {
+    KnowzeTheme {
+        VerificationScreen()
+    }
+}
