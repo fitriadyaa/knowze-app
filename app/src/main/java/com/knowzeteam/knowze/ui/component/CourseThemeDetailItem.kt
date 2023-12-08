c3acfa58b345a37cbc48fa79644af85f7b0037a2
Will-Lim1 <will.lim2002@gmail.com>
2023-12-08 21:35:31 +0700

    Removed unused Imports

--- app/src/main/java/com/knowzeteam/knowze/ui/component/CourseThemeDetailItem.kt
+++ app/src/main/java/com/knowzeteam/knowze/ui/component/CourseThemeDetailItem.kt
@@ -1,7 +1,6 @@
 package com.knowzeteam.knowze.ui.component
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
--- app/src/main/java/com/knowzeteam/knowze/ui/component/CourseThemeItem.kt
+++ app/src/main/java/com/knowzeteam/knowze/ui/component/CourseThemeItem.kt
@@ -1,11 +1,8 @@
 package com.knowzeteam.knowze.ui.component
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
