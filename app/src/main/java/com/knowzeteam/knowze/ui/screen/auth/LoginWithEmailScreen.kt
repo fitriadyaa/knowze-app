af0e2b29151f1990619e64207c7e09d4c9ff85a5
Will-Lim1 <will.lim2002@gmail.com>
2023-12-08 16:53:18 +0700

    [Register Screen] Slicing UI & Removed unused Imports

--- app/src/main/java/com/knowzeteam/knowze/ui/screen/auth/LoginWithEmailScreen.kt
+++ app/src/main/java/com/knowzeteam/knowze/ui/screen/auth/LoginWithEmailScreen.kt
@@ -1,6 +1,5 @@
 package com.knowzeteam.knowze.ui.screen.auth
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -9,30 +8,19 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.ClickableText
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
---
+++ app/src/main/java/com/knowzeteam/knowze/ui/screen/auth/RegisterScreen.kt
@@ -0,0 +1,159 @@
+package com.knowzeteam.knowze.ui.screen.auth
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowLeft
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.knowzeteam.knowze.R
+import com.knowzeteam.knowze.ui.component.EmailTextField
+import com.knowzeteam.knowze.ui.component.NameTextField
+import com.knowzeteam.knowze.ui.component.PasswordTextField
+import com.knowzeteam.knowze.ui.theme.KnowzeTheme
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun RegisterScreen(
+    modifier: Modifier = Modifier
+) {
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = {
+                    Text(
+                        text = stringResource(id = R.string.register),
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
+                    )
+                },
+                navigationIcon = {
+                    IconButton(onClick = { /* do something */ }) {
+                        Icon(
+                            imageVector = Icons.Filled.KeyboardArrowLeft,
+                            contentDescription = "Localized description"
+                        )
+                    }
+                }
+            )
+        },
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = modifier.fillMaxSize()
+        ) {
+            Box {
+                Column {
+                    Text(
+                        text = stringResource(id = R.string.write_your_name_here),
+                        style = MaterialTheme.typography.bodyMedium.copy(
+                            fontSize = 14.sp,
+                            fontWeight = FontWeight.Light,
+                            textAlign = TextAlign.Start
+                        )
+                    )
+
+                    NameTextField(label = stringResource(id = R.string.your_name))
+
+                    Spacer(modifier = Modifier.height(30.dp))
+
+                    Text(
+                        text = stringResource(id = R.string.email),
+                        style = MaterialTheme.typography.bodyMedium.copy(
+                            fontSize = 14.sp,
+                            fontWeight = FontWeight.Light,
+                            textAlign = TextAlign.Start
+                        )
+                    )
+
+                    EmailTextField(label = stringResource(id = R.string.email_example))
+
+                    Spacer(modifier = Modifier.height(25.dp))
+
+                    Text(
+                        text = stringResource(id = R.string.password),
+                        style = MaterialTheme.typography.bodyMedium.copy(
+                            fontSize = 14.sp,
+                            fontWeight = FontWeight.Light,
+                            textAlign = TextAlign.Start
+                        )
+                    )
+
+                    PasswordTextField(label = stringResource(id = R.string.password_example))
+
+                    Spacer(modifier = Modifier.height(25.dp))
+
+                    Text(
+                        text = stringResource(id = R.string.repeat_password),
+                        style = MaterialTheme.typography.bodyMedium.copy(
+                            fontSize = 14.sp,
+                            fontWeight = FontWeight.Light,
+                            textAlign = TextAlign.Start
+                        )
+                    )
+
+                    PasswordTextField(label = stringResource(id = R.string.password_example))
+                }
+            }
+
+            Spacer(modifier = Modifier.height(50.dp))
+
+            RegisterButton(onClick = { /*TODO*/ })
+        }
+    }
+}
+
+@Composable
+fun RegisterButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Button(
+        onClick = onClick,
+        shape = RoundedCornerShape(10.dp),
+        modifier = modifier
+            .fillMaxWidth()
+            .height(60.dp),
+        colors = ButtonDefaults.buttonColors(MaterialTheme.colorScheme.primary)
+    ) {
+        Text(
+            text = stringResource(R.string.register),
+            modifier = modifier.padding(start = 8.dp)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun RegisterPreview() {
+    KnowzeTheme {
+        RegisterScreen()
+    }
+}
